767ddf466d3ee2e092d5bd0319ac09f0bb2c2f6a
IrakozeLoraine <mukezwa@gmail.com>
2021-07-26 14:00:13 +0200

    Removing Custom exception

--- src/main/java/Stack.java
+++ src/main/java/Stack.java
@@ -27,6 +27,13 @@ public class Stack<T> {
         return size() == 0;
     }
 
+    //Peek the top of the stack
+    public T peek() {
+        if (isEmpty())
+            throw new RuntimeException("Stack is Empty");
+        return stack.peekLast();
+    }
+
     //Push an element to stack
     public void push(T element)
     {
@@ -34,24 +41,9 @@ public class Stack<T> {
     }
 
     //Pop an element off the stack and throw underflow error when empty
-    public T pop() throws CustomException {
+    public T pop() {
         if(isEmpty())
-            throw new CustomException("Stack is empty");
+            throw new RuntimeException("Stack is empty");
         return stack.removeLast();
-    }
-
-    //Peek the top of the stack
-    public T peek() throws CustomException {
-        if (isEmpty())
-            throw new CustomException("Stack is Empty");
-        return stack.peekLast();
-    }
-}
-
-class CustomException extends Exception {
-    String message;
-    CustomException(String message)
-    {
-        this.message = message;
     }
 }
